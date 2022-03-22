703d8c15ae4a647774b2c702f60cda1d497c376a
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-03-22 11:07:16 +0700

    rahman complete api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,18 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function(){
-    return Product::all();
-});
+Route::resource('products', ProductController::class); 
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-Route::post('/products', function(){
-    return Product::create([
-        'name' => 'product One',
-        'slug' => 'product-One',
-        'description' => 'This Is Product One',
-        'price' => '99.99'
-    ]);
-});
+// Route::get('/products', [ProductController::class, 'index']);
+// Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
