84a42b1eacdee677b75ff38702f133d46f81c372
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-03-24 11:55:36 +0700

    daftar user dan ambil token

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,16 +15,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Route::resource('products', ProductController::class); 
 
-Route::resource('products', ProductController::class); 
-// Route::get('/products/search/{name}', [ProductController::class, 'search']);
+//public routes
+Route::get('/register', [AuthController::class, 'register']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-Route::middleware('auth:sanctum')->get('/user', function () {
-    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+// protected routes
+Route::group(['middleware' => ['auth:scantum']] , function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
-
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
